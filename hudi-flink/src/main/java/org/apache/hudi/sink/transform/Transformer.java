607741fecd5283ffedbb2901e2cdeace4022eda1
Alex Barreto <axbarreto@lavabit.com>
2021-07-19 11:35:39 -0400

    [HUDI-2193] Remove state in BootstrapFunction

--- hudi-flink/src/main/java/org/apache/hudi/sink/transform/Transformer.java
+++ hudi-flink/src/main/java/org/apache/hudi/sink/transform/Transformer.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,8 +16,20 @@
  * limitations under the License.
  */
 
-package org.apache.hudi.table.action.commit;
+package org.apache.hudi.sink.transform;
 
-public enum BucketType {
-  UPDATE, INSERT
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.table.data.RowData;
+
+/**
+ * Transform source stream to target stream before writing.
+ */
+public interface Transformer {
+
+  /**
+   * Transform source DataStream to target DataStream.
+   * @param source
+   */
+  DataStream<RowData> apply(DataStream<RowData> source);
+
 }
